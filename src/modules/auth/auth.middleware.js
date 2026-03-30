2f273184a9124cc520ac3991428a4a00f03dab05
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2026-03-30 21:26:54 +0600

    implement authentication and authorization middleware for user access control

--- src/modules/auth/auth.middleware.js
+++ src/modules/auth/auth.middleware.js
@@ -1,1 +1,43 @@
-console.log('middleware');
+import ApiError from "../../common/utils/api-error";
+import { verifyAccessToken } from "../../common/utils/jwt.utils";
+import User from "./auth.model";
+
+const authenticate = async(req , res , next) => {
+
+    let token;
+    if(req.header.authorization?.startWith("Bearer")){
+        token = eq.header.authorization.split(" ")[1];
+    }
+
+    if(!token) throw ApiError.unauthorize("No Authenticate")
+    
+    // token verify 
+    const decode =  verifyAccessToken(token)
+    const user = await User.findOne(decode.id)
+
+    if(!user) throw ApiError.unauthorize("Not valid user ")
+    
+    req.user = {
+        id : user?._id,
+        email : user?.email,
+        role : user?.role,
+        name : user?.name
+    }
+
+    next();
+
+}
+
+const authorize = async(...role) => {
+    return (req , res , next) => {
+
+        if(!role.includes(req.user?.role)){
+            throw ApiError.forbidden("You do not have permission bag shala")
+        }
+
+        next();
+
+    }
+}
+
+export { authenticate, authorize };
